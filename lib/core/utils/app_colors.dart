664d25579a7918fc277ff5763cf903928cca88f4
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-04-03 15:15:13 -0700

    Create App Colors

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color main = Color(0xffD3E97A);
+  static const Color grey = Color(0xffC7C7C7);
+  static const Color background = Color(0xff000000);
+  static const Color white = Color(0xffffffff);
+}
