0415f3eb9cf91978d1633d434d0be4789f9ed27f
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-04-04 13:22:24 -0700

    Create Custom Button

---
+++ lib/core/widgets/custom_button.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import '../utils/app_colors.dart';
+import '../utils/app_styles.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({
+    super.key,
+    required this.text,
+    this.showDotIcon = true,
+  });
+  final String text;
+  final bool? showDotIcon;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      onPressed: () {},
+      color: AppColors.main,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16),
+        side: const BorderSide(color: AppColors.main),
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        spacing: 16,
+        children: [
+          Text(
+            text,
+            style: AppStyles.bold16(context),
+          ),
+          showDotIcon == true
+              ? Icon(
+                  Icons.hdr_strong,
+                  size: 15,
+                  color: AppColors.black,
+                )
+              : SizedBox(width: 0, height: 0),
+        ],
+      ),
+    );
+  }
+}
