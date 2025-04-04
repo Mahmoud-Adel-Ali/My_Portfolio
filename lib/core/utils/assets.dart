ae65b4f38213e1bbb1f2a8ff9f159a3aabe3d695
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-04-03 22:35:04 -0700

    Add Some Assets ( Icons )

---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  static const String iconsFacebook = "assets/icons/facebook.svg";
+
+  static const String iconsGithub = "assets/icons/github.svg";
+
+  static const String iconsInstagram = "assets/icons/instagram.svg";
+
+  static const String iconsLinkedin = "assets/icons/linkedin.svg";
+
+  static const String iconsVector = "assets/icons/Vector.svg";
+}
+
