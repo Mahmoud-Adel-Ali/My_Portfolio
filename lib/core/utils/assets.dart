3afef5afc09dc24f6b98fbdb31099ecc38700792
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-04-04 13:50:18 -0700

    Add Porjects Thumbnail (8 projects)

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -11,5 +11,21 @@ class Assets {
   static const String iconsLinkedin = "assets/icons/linkedin.svg";
 
   static const String iconsVector = "assets/icons/Vector.svg";
+
+  static const String imagesDocLink = "assets/images/doc_link.png";
+
+  static const String imagesInstagram = "assets/images/instagram.png";
+
+  static const String imagesNectar = "assets/images/nectar.png";
+
+  static const String imagesOpenAi = "assets/images/open_ai.png";
+
+  static const String imagesPaymentFeature = "assets/images/payment_feature.png";
+
+  static const String imagesQuickMart = "assets/images/quick_mart.png";
+
+  static const String imagesResponsiveDashboard = "assets/images/responsive_dashboard.png";
+
+  static const String imagesTicTacToe = "assets/images/tic_tac_toe.png";
 }
 
