0a09ae1a6d51cc7a0578c22ed43e3b48243ac6e1
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-04-05 21:50:26 -0700

    Create Custom Drawer for mobile layout [Done]

--- lib/features/portfolio/presentation/views/widgets/custom_drawer.dart
+++ lib/features/portfolio/presentation/views/widgets/custom_drawer.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:my_portfolio/core/widgets/custom_app_bar_actions_item.dart';
 
 import '../../../../../core/utils/app_colors.dart';
 import '../../../../../core/utils/app_styles.dart';
+import 'custom_drawer_item.dart';
 
 class CustomDrawer extends StatelessWidget {
   const CustomDrawer({super.key});
@@ -12,54 +12,33 @@ class CustomDrawer extends StatelessWidget {
     return Drawer(
       backgroundColor: AppColors.background,
       width: 200,
-      child: Column(
-        children: [
-          DrawerHeader(
-            child: Center(
-              child: Text(
-                'Mahmoud Adel.',
-                style: AppStyles.regular40(context),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            DrawerHeader(
+              child: Center(
+                child: Text(
+                  'Mahmoud Adel.',
+                  style: AppStyles.regular40(context),
+                ),
               ),
             ),
-          ),
-          CustomDrawerItem(
-            text: "Work",
-            onPressed: () {},
-          ),
-          CustomDrawerItem(
-            text: "About",
-            onPressed: () {},
-            iconData: Icons.info,
-          ),
-          CustomDrawerItem(
-            text: "Contact",
-            onPressed: () {},
-            iconData: Icons.email,
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class CustomDrawerItem extends StatelessWidget {
-  const CustomDrawerItem({
-    super.key,
-    required this.text,
-    this.onPressed,
-    this.iconData = Icons.work,
-  });
-
-  final String text;
-  final Function()? onPressed;
-  final IconData? iconData;
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      leading: Icon(iconData),
-      title: CustomAppBarActionsItem(
-        text: text,
-        onPressed: onPressed,
+            CustomDrawerItem(
+              text: "Work",
+              onPressed: () {},
+            ),
+            CustomDrawerItem(
+              text: "About",
+              onPressed: () {},
+              iconData: Icons.info,
+            ),
+            CustomDrawerItem(
+              text: "Contact",
+              onPressed: () {},
+              iconData: Icons.email,
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/features/portfolio/presentation/views/widgets/custom_drawer_item.dart
@@ -0,0 +1,27 @@
+
+import 'package:flutter/material.dart';
+
+import '../../../../../core/widgets/custom_app_bar_actions_item.dart';
+
+class CustomDrawerItem extends StatelessWidget {
+  const CustomDrawerItem({
+    super.key,
+    required this.text,
+    this.onPressed,
+    this.iconData = Icons.work,
+  });
+
+  final String text;
+  final Function()? onPressed;
+  final IconData? iconData;
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Icon(iconData),
+      title: CustomAppBarActionsItem(
+        text: text,
+        onPressed: onPressed,
+      ),
+    );
+  }
+}
