d4279753240c3924b96f4c47e0f6e1bf6fd660eb
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-04-07 08:58:21 -0700

    Create Custom Link Button

--- lib/core/widgets/custom_link_button.dart
+++ lib/core/widgets/custom_link_button.dart
@@ -6,29 +6,26 @@ import 'package:flutter_svg/flutter_svg.dart';
 import '../utils/app_colors.dart';
 import '../utils/app_styles.dart';
 
-class CustomTextButton extends StatelessWidget {
+class CustomLinkButton extends StatelessWidget {
   final String text;
   final String svgImage;
   final String link;
-  final VoidCallback? onPressed;
 
-  const CustomTextButton({
+  const CustomLinkButton({
     super.key,
     required this.text,
     required this.svgImage,
     required this.link,
-    this.onPressed,
   });
 
   @override
   Widget build(BuildContext context) {
     return Flexible(
       child: TextButton(
-        onPressed: onPressed ??
-            () {
-              // Handle link opening logic here
-              log('Opening link: $link');
-            },
+        onPressed: () {
+          // Handle link opening logic here
+          log('Opening link: $link');
+        },
         style: TextButton.styleFrom(
           padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
           tapTargetSize: MaterialTapTargetSize.shrinkWrap,
@@ -52,6 +49,7 @@ class CustomTextButton extends StatelessWidget {
               textAlign: TextAlign.center,
               style: AppStyles.bold16(context).copyWith(
                 color: AppColors.main,
+                decoration: TextDecoration.underline,
               ),
             ),
           ],
--- lib/features/portfolio/presentation/views/widgets/project_links.dart
+++ lib/features/portfolio/presentation/views/widgets/project_links.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:my_portfolio/core/widgets/custom_text_button.dart';
+import 'package:my_portfolio/core/widgets/custom_link_button.dart';
 
 import '../../../../../core/models/project_model.dart';
 import '../../../../../core/utils/assets.dart';
@@ -15,21 +15,21 @@ class ProjectLinks extends StatelessWidget {
     return SingleChildScrollView(
       child: Row(
         children: [
-          CustomTextButton(
+          CustomLinkButton(
             text: 'GitHub',
             svgImage: Assets.iconsGithub,
             link: project.gitHubLink,
           ),
           project.linkedInLink == null
               ? const SizedBox()
-              : CustomTextButton(
+              : CustomLinkButton(
                   text: 'LinkedIn',
                   svgImage: Assets.iconsLinkedin,
                   link: project.linkedInLink!,
                 ),
           project.linkedInLink == null
               ? const SizedBox()
-              : CustomTextButton(
+              : CustomLinkButton(
                   text: 'Figma',
                   svgImage: Assets.iconsFigma,
                   link: project.linkedInLink!,
