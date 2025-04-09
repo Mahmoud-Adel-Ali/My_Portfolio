5993f05b7f0244f9c02a50e414e26322f1697562
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-04-09 09:28:07 -0700

    Add Skills svg Images

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -2,15 +2,43 @@
 class Assets {
   Assets._();
   
+  static const String iconsBloc = "assets/icons/bloc.svg";
+
+  static const String iconsCleanArch = "assets/icons/clean_arch.svg";
+
+  static const String iconsCleanCode = "assets/icons/clean_code.svg";
+
+  static const String iconsDart = "assets/icons/dart.svg";
+
+  static const String iconsDebugging = "assets/icons/debugging.svg";
+
+  static const String iconsDesignPatterns = "assets/icons/design_patterns.svg";
+
   static const String iconsFacebook = "assets/icons/facebook.svg";
 
   static const String iconsFigma = "assets/icons/figma.svg";
+
+  static const String iconsFirebase = "assets/icons/firebase.svg";
+
+  static const String iconsFlutter = "assets/icons/flutter.svg";
+
+  static const String iconsGit = "assets/icons/git.svg";
 
   static const String iconsGithub = "assets/icons/github.svg";
 
   static const String iconsInstagram = "assets/icons/instagram.svg";
 
   static const String iconsLinkedin = "assets/icons/linkedin.svg";
+
+  static const String iconsMvvm = "assets/icons/mvvm.svg";
+
+  static const String iconsOop = "assets/icons/oop.svg";
+
+  static const String iconsSolidPrinciples = "assets/icons/solid_principles.svg";
+
+  static const String iconsStateManagement = "assets/icons/state_management.svg";
+
+  static const String iconsTesting = "assets/icons/testing.svg";
 
   static const String iconsVector = "assets/icons/Vector.svg";
 
