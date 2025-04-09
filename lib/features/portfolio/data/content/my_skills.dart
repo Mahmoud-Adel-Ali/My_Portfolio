7deea137d53de6f7f2e5c1212cd0eac7a21d3a12
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-04-09 09:28:43 -0700

    Add Skills List Data

---
+++ lib/features/portfolio/data/content/my_skills.dart
@@ -0,0 +1,19 @@
+import '../../../../core/utils/assets.dart';
+import '../models/skill_model.dart';
+
+List<SkillModel> mySkills = [
+  SkillModel(label: "Flutter", svgImage: Assets.iconsFlutter),
+  SkillModel(label: "Dart", svgImage: Assets.iconsDart),
+  SkillModel(label: "Firebase", svgImage: Assets.iconsFirebase),
+  SkillModel(label: "Git", svgImage: Assets.iconsGit),
+  SkillModel(label: "MVVM", svgImage: Assets.iconsMvvm),
+  SkillModel(label: "Clean Architcture", svgImage: Assets.iconsCleanArch),
+  SkillModel(label: "Clean Code", svgImage: Assets.iconsCleanCode),
+  SkillModel(label: "OOP", svgImage: Assets.iconsOop),
+  SkillModel(label: "Solid Principles", svgImage: Assets.iconsSolidPrinciples),
+  SkillModel(label: "State Management", svgImage: Assets.iconsStateManagement),
+  SkillModel(label: "BloC", svgImage: Assets.iconsBloc),
+  SkillModel(label: "Design Pattern", svgImage: Assets.iconsDesignPatterns),
+  SkillModel(label: "Testing", svgImage: Assets.iconsTesting),
+  SkillModel(label: "Debugging", svgImage: Assets.iconsDebugging),
+];
