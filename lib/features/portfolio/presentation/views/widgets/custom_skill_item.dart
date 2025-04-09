d48fc25349a489c11668fe66eee5d702d098809d
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-04-09 09:27:21 -0700

    Create Custom Skill Item

---
+++ lib/features/portfolio/presentation/views/widgets/custom_skill_item.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:my_portfolio/core/utils/app_styles.dart';
+
+import '../../../../../core/utils/app_colors.dart';
+import '../../../data/models/skill_model.dart';
+
+class CustomSkillItem extends StatelessWidget {
+  final SkillModel skill;
+
+  const CustomSkillItem({
+    super.key,
+    required this.skill,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      decoration: BoxDecoration(
+        color: AppColors.black,
+        borderRadius: BorderRadius.circular(24),
+        border: Border.all(color: AppColors.black2),
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          SvgPicture.asset(skill.svgImage),
+          const SizedBox(width: 4),
+          Text(
+            skill.label,
+            style: AppStyles.medium14(context),
+          ),
+        ],
+      ),
+    );
+  }
+}
