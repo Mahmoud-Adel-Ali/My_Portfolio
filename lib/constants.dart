7b40cfbb731223bd98c546a7179a779f1562e87f
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-04-13 05:39:32 -0700

    Add My Links

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,4 +2,11 @@ abstract class Constants {
   static const String bebasNeueFontFamily = "BebasNeue";
   static const String manropeFontFamily = "Manrope";
   static const String interFontFamily = "Inter";
+  static const String linkedInUrl =
+      "https://www.linkedin.com/in/mahmoud-adel-ali-mohamed/";
+  static const String githubUrl = "https://github.com/Mahmoud-Adel-Ali";
+  static const String facebookUrl =
+      "https://www.facebook.com/profile.php?id=100035383894778";
+  static const String resumeUrl =
+      "https://sohagedu-my.sharepoint.com/:w:/g/personal/mahmoud2100062_fci_sohag_edu_eg/EYZ4ECvgto9Bje6ALjEKNDUBTXh4PXXBIyJ1QS56gk476g";
 }
