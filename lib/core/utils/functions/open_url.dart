5095e093eac4e1277af4c6d13de640e084ecc807
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-04-14 12:36:37 -0700

    Refactor openUrl Methode

--- lib/core/utils/functions/open_url.dart
+++ lib/core/utils/functions/open_url.dart
@@ -4,13 +4,22 @@ import 'package:url_launcher/url_launcher.dart';
 Future<void> openUrl(BuildContext context, String url) async {
   final Uri uri = Uri.parse(url);
 
-  if (await canLaunchUrl(uri)) {
-    await launchUrl(uri);
-  } else {
+  try {
+    final bool launched = await launchUrl(uri);
+    if (!launched) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Could not launch the URL'),
+          backgroundColor: Colors.red,
+        ),
+      );
+      // throw 'Could not launch the URL';
+    }
+  } catch (e) {
     // ignore: use_build_context_synchronously
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
-        content: Text('Could not launch the URL'),
+        content: Text(e.toString()),
         backgroundColor: Colors.red,
       ),
     );
