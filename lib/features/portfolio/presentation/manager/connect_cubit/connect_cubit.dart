deb1b46674fd75297bd3c69983997bb95520c079
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-04-17 05:05:05 -0700

    Change My Resume Link

--- lib/features/portfolio/presentation/manager/connect_cubit/connect_cubit.dart
+++ lib/features/portfolio/presentation/manager/connect_cubit/connect_cubit.dart
@@ -1,5 +1,9 @@
+import 'dart:developer';
+
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:mailer/mailer.dart';
+import 'package:mailer/smtp_server.dart';
 
 import 'connect_state.dart';
 
@@ -12,5 +16,37 @@ class ConnectCubit extends Cubit<ConnectState> {
   TextEditingController messageConroller = TextEditingController();
   GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
-  void sendMessage() {}
+ void sendMessage() async {
+  try {
+    emit(SendMessageLoading());
+
+    // Developer uses Gmail account and app password
+    String username = emailConroller.text; // your Gmail address
+    String password = 'kboz xmxf rwfb umfw'; // your generated Gmail app password
+
+    // Developer uses smtpServer with Gmail configuration
+    final smtpServer = gmail(username, password);
+
+    // Using the data you provided
+    String recipientEmail = "mamali@gmail.com";
+    String recipientName = "Mam Ali";
+
+    // Developer constructs the message to be sent
+    final message = Message()
+      ..from = Address(username, nameConroller.text)
+      ..recipients.add(Address(recipientEmail, recipientName))
+      ..subject = subjectConroller.text
+      ..text = messageConroller.text;
+
+    // Developer sends the message using the SMTP server
+    final sendReport = await send(message, smtpServer);
+
+    log('Message sent: $sendReport');
+    emit(SendMessageSuccess());
+  } catch (e) {
+    emit(SendMessageFailure(message: e.toString()));
+  }
+}
+
+//
 }
