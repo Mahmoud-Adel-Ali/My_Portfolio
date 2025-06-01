8b448be5b54b6401e8383cc4076c1c294ca8d2b0
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-06-01 10:31:03 -0700

    Add Device Preview Package

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
@@ -5,5 +7,10 @@ import 'portfolio.dart';
 
 Future<void> main() async {
   await dotenv.load(fileName: ".env");
-  runApp(const Portfolio());
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => const Portfolio(), // Wrap your app
+    ),
+  );
 }
