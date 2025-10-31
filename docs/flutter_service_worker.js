34d2dc5d45a29318371982ec2cfcb6e63016f4b8
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-10-30 19:21:15 -0700

    Fix GitHub Pages base href and script

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -63,7 +63,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "232073512bbd557171892d2594c098ae
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "517227e32784100a007c4bc3ccbb251a",
+"flutter_bootstrap.js": "b913f00cbadd073e3fbc8314760b86ce",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
