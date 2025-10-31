1a1f63dc57ebce8489ab942c4b44a2493c04a603
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-10-30 18:42:06 -0700

    Deploy web build to docs II

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -63,7 +63,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "232073512bbd557171892d2594c098ae
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "a88c0e03b971e78551dbb617f7ed3027",
+"flutter_bootstrap.js": "517227e32784100a007c4bc3ccbb251a",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
